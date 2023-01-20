7d7881ed2541b83087268cf2e910d9f1f11534e1
Bastien Tafforeau <bastien.tafforeau@gmail.com>
2023-01-20 17:00:22 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="banner" role="banner">
+    <div class="banner-container">
+      <a class="banner-logo" href="/">
+        <img src="../assets/logo.png" alt="btafforeau logo"/>
+      </a>
+      <div class="banner-menu">
+        <ul>
+          <li>Lorem</li>
+          <li>Ipsum</li>
+          <li>Dolor</li>
+          <li>Sit</li>
+          <li>Amet</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp"
+}
+</script>
+
+<style scoped>
+  .banner {
+    align-items: center;
+    display: flex;
+    padding: 30px 0;
+    position: absolute;
+    width: 100%;
+  }
+
+  .banner-container {
+    padding: 0 30px;
+    width: 100%;
+  }
+
+  .banner-logo {
+    display: block;
+    float: left;
+    position: relative;
+  }
+
+  .banner-logo img {
+    display: block;
+    max-height: 50px;
+    position: relative;
+  }
+
+  .banner-menu ul {
+    float: right;
+    list-style: none;
+    position: relative;
+  }
+
+  .banner-menu li {
+    float: left;
+    position: relative;
+    padding-left: 30px;
+  }
+</style>
