b4844ca6f3618151439d0f34ecb0c5d64fcc7c90
Bastien Tafforeau <bastien.tafforeau@gmail.com>
2023-01-21 16:09:07 +0100

    Cleaning main js file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).use(VueRouter)
 createApp(App).use(router).mount('#app')
